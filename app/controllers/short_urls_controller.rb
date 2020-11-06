90c3af28006040efd8880722d55b08cf1a1899b5
github username <github email address>
2020-11-05 19:28:06 -0500

    Render json in index method and validate the presence of full_url

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,12 +4,20 @@ class ShortUrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    @short_urls = ShortUrl.all
+    render json: @short_urls
   end
 
   def create
   end
 
   def show
+  end
+
+  private
+
+  def short_url_params
+    params.permit(:full_url, :title, :click_count)
   end
 
 end
